1eaf525be51c7f29a30e6ec357fb89b1c2017d9a
Telaviga <telaviga@gmail.com>
2023-01-16 20:40:29 +0300

    16.01.2023

---
+++ project/c/stepik/c_progtamming/c1.3.c
@@ -0,0 +1,15 @@
+1#include <stdio.h>
+	if (42){ //выведет Hello!
+		printf("Hello!");
+	} else {
+		printf("No good");
+	}
+
+	if (42-42){ //выведет Not good
+		printf("Hello!");
+	} else {
+		printf("Not good");
+	}
+	void main();
+	return 0;
+
